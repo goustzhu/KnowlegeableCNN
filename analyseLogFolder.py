4de1adefbb85b109e0f092949da663e72645fa67
okcd00 <okcd00@qq.com>
2016-04-28 22:20:27 +0800

    Update analyseLogFolder
    Divide Stdout, and Turn "Object Transfer“ into "Directly Assignment" by
    "set.default(key, defaultObj)"

--- analyseLogFolder.py
+++ analyseLogFolder.py
@@ -2,13 +2,16 @@ import os
 import codecs
 import string
 
+import logMod
+l = logMod.logMod()
+
 def analyseLogFolder(path, label="1"):
-    print "Started!"
+    l.Notice("Analysis LogFolder Started.")
     base_dir = path + "/" + label
     dir_list = os.listdir(base_dir)
     
-    cell_word_statistic = dict()
     cell_scores = dict()
+    cell_word_statistic = dict()
     
     dir_count = 0
     for d in dir_list:
@@ -17,15 +20,11 @@ def analyseLogFolder(path, label="1"):
         nn_num = 0
         while True:
             nn_file = nn_dir + str(nn_num) + "_merged"
-#             print "Analyzing ", nn_file
-            print ".",
             if os.path.exists(nn_file):
                 with codecs.open(nn_file, "r", "utf-8", "ignore") as f:
                     cell_score = string.atof(f.readline().strip().split(" ")[1])
-                    if not nn_num in cell_scores.keys():
-                        cell_scores[nn_num] = cell_score
-                    else:
-                        cell_scores[nn_num] += cell_score
+                    cell_scores.setdefault(nn_num, 0)
+                    cell_scores[nn_num] += cell_score
                     
                     f.readline()
                     # deal with nn-num cell
@@ -33,24 +32,23 @@ def analyseLogFolder(path, label="1"):
                         word, score = line.strip().split("\t")
                         score = string.atof(score)
                         
-                        if not nn_num in cell_word_statistic.keys():
-                            cell_state = dict()
-                            cell_word_statistic[nn_num] = cell_state
-                        else:
-                            cell_state = cell_word_statistic[nn_num]
+                        __dict = dict()
+                        cell_word_statistic.setdefault(nn_num, __dict)                        
+                        cell_state = cell_word_statistic[nn_num]
                         
-                        if word in cell_state.keys(): 
-                            word_state = cell_state[word]
-                            word_state[0] += score
-                            word_state[1] += 1 
-                        else:
-                            cell_state[word] = [score, 1]
-            else:
-                break
+                        cell_state.setdefault(word, [0, 0])
+                        cell_state[word][0] += score
+                        cell_state[word][1] += 1
+                        
+            else: break
             nn_num += 1
+            print "R",
+            if nn_num%20==0: print
         dir_count += 1
-    print "Wirting result."
-    analyse_base_dir = path + "/analyse_" + label + "/"     
+        
+    print
+    print "Writing result:"
+    analyse_base_dir = path + "/analyse_" + label + "/"
     if not os.path.exists(analyse_base_dir):
         os.makedirs(analyse_base_dir)
     
@@ -59,9 +57,10 @@ def analyseLogFolder(path, label="1"):
         cell_scores_list.sort(key=lambda x:x[1], reverse=True)
         for c, score in cell_scores_list:
             f.write("%d\t%f\n" % (c, score))
-            
+    
     for (cell, cell_state) in cell_word_statistic.items():
-        print "Wring cell: ", str(cell)
+        print "W",
+        if (cell+1)%20==0: print
         with codecs.open(analyse_base_dir + str(cell), "w", "utf-8", "ignore") as f:
             word_list = map(lambda (k, v): (k, v[0] / v[1]) , list(cell_state.items()))
             word_list.sort(key=lambda x:x[1], reverse=True)
